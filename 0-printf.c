783b1ac9737f33910d0d800b73af4c822c917014
Lensatom <ayomidetommiwa@gmail.com>
2022-10-18 11:33:58 -0700

    Hello

---
+++ 0-printf.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _printf - A funtion that works just like printf
+ * @temp: The output template
+ * @args: The place holder arg
+ * Return: Always nothing
+ */
+
+void _printf(int temp, int args, ...)
+{
+}
---
+++ main.h
@@ -0,0 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+_printf(int temp, int args, ...);
+
+#endif
